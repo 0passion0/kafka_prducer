31a4a1677d1ddf61ac7f0dde094b3589bdf08eb0
tongliga <tongliga@medpeer.cn>
2025-09-02 13:28:31 +0800

    refactor(application): 优化代码结构和模型定义
    - 移除了 file_cursor.py 中未使用的 json导入
    - 更新了 information_data_structure.py 中的导入语句- 在 DataPayload 类中添加了 PyCompatibility 注释
    - 删除了 DataPayload 类中未使用的 info_source 字段

--- application/models/kafka_models/information_data_structure.py
+++ application/models/kafka_models/information_data_structure.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
-from typing import List, Dict, Any, Optional
+from typing import List, Any, Optional
 from pydantic import BaseModel, Field
 
 from application.models.kafka_models.base_data_structure import DataStructure
 
 
 # ---------- 子模型：data ----------
+# noinspection PyCompatibility
 class DataPayload(BaseModel):
     """
     核心资讯数据模型
@@ -14,7 +15,6 @@ class DataPayload(BaseModel):
         info_date (str): 资讯发布日期，格式如 'YYYY-MM-DD'
         info_section (List[str]): 资讯正文段落
         info_author (str): 资讯作者
-        info_source (str): 资讯来源
         description (str): 资讯描述
     """
     info_date: Optional[str] = None
